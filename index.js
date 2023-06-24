2d159ec7829d6bd1ba02a27a6a7b6ed7c097bcb7
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2023-06-24 09:01:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const compression = require('compression');
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: '*',
+    origin: 'https://64969582f4f5742f799b5a2c--geteatingapp.netlify.app',
 };
 app.use(cors(corsOptions));
 
